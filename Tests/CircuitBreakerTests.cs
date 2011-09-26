b5717fc7d2e8ac78407724965cfffc4c2640182e
Tatham Oddie <tatham@oddie.com.au>
2011-09-26 23:46:58 +1000

    Added service level tests.

---
+++ Tests/CircuitBreakerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using ReliabilityPatterns;
+
+namespace Tests
+{
+    [TestFixture]
+    public class CircuitBreakerTests
+    {
+        [Test]
+        public void ShouldInitializeWithServiceLevel100()
+        {
+            var circuitBreaker = new CircuitBreaker();
+            Assert.AreEqual(100, circuitBreaker.ServiceLevel);
+        }
+
+        [Test]
+        public void ShouldReduceServiceLevelWithFailure()
+        {
+            var circuitBreaker = new CircuitBreaker();
+
+            try { circuitBreaker.Execute(() => { throw new ApplicationException(); }); }
+            catch (OperationFailedException) { }
+
+            Assert.AreEqual(80, circuitBreaker.ServiceLevel);
+        }
+
+        [Test]
+        public void ShouldReduceServiceLevelWithConsecutiveFailures()
+        {
+            var circuitBreaker = new CircuitBreaker();
+
+            try { circuitBreaker.Execute(() => { throw new ApplicationException(); }); }
+            catch (OperationFailedException) { }
+
+            try { circuitBreaker.Execute(() => { throw new ApplicationException(); }); }
+            catch (OperationFailedException) { }
+
+            Assert.AreEqual(60, circuitBreaker.ServiceLevel);
+        }
+    }
+}
