5155e3347cda267fb7cfb3d154d04de474d55353
Tatham Oddie <tatham@oddie.com.au>
2011-09-27 00:14:05 +1000

    Refactored service level tests into one data driven one.

--- Tests/CircuitBreakerTests.cs
+++ Tests/CircuitBreakerTests.cs
@@ -8,35 +8,35 @@ namespace Tests
     public class CircuitBreakerTests
     {
         [Test]
-        public void ShouldInitializeWithServiceLevel100()
-        {
-            var circuitBreaker = new CircuitBreaker();
-            Assert.AreEqual(100, circuitBreaker.ServiceLevel);
-        }
-
-        [Test]
-        public void ShouldReduceServiceLevelWithFailure()
-        {
-            var circuitBreaker = new CircuitBreaker();
-
-            try { circuitBreaker.Execute(() => { throw new ApplicationException(); }); }
-            catch (OperationFailedException) { }
-
-            Assert.AreEqual(80, circuitBreaker.ServiceLevel);
-        }
-
-        [Test]
-        public void ShouldReduceServiceLevelWithConsecutiveFailures()
+        [TestCase("", Result = 100d)]
+        [TestCase("bad", Result = 80d)]
+        [TestCase("bad good", Result = 100d)]
+        [TestCase("bad bad", Result = 60d)]
+        [TestCase("bad bad good", Result = 80d)]
+        [TestCase("bad bad good good", Result = 100d)]
+        [TestCase("bad good bad good", Result = 100d)]
+        public double ServiceLevel(string callPattern)
         {
             var circuitBreaker = new CircuitBreaker();
 
-            try { circuitBreaker.Execute(() => { throw new ApplicationException(); }); }
-            catch (OperationFailedException) { }
+            foreach (var call in callPattern.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                switch (call)
+                {
+                    case "good":
+                        circuitBreaker.Execute(() => { });
+                        break;
+                    case "bad":
+                        try { circuitBreaker.Execute(() => { throw new Exception(); }); }
+                        catch (OperationFailedException) { }
+                        break;
+                    default:
+                        Assert.Fail("Unknown call sequence");
+                        break;
+                }
+            }
 
-            try { circuitBreaker.Execute(() => { throw new ApplicationException(); }); }
-            catch (OperationFailedException) { }
-
-            Assert.AreEqual(60, circuitBreaker.ServiceLevel);
+            return circuitBreaker.ServiceLevel;
         }
     }
 }
