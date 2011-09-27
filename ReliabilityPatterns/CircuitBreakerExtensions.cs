b896f9b82881d09e8d29afecf7f30cff99f94392
Tatham Oddie <tatham@oddie.com.au>
2011-09-27 13:48:56 +1000

    Changed ExecuteWithRetries to throw an AggregateException at the end with all failures.

--- ReliabilityPatterns/CircuitBreakerExtensions.cs
+++ ReliabilityPatterns/CircuitBreakerExtensions.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace ReliabilityPatterns
@@ -8,14 +10,21 @@ namespace ReliabilityPatterns
         public static void ExecuteWithRetries(this CircuitBreaker circuitBreaker, Action operation, ushort allowedRetries, TimeSpan retryInterval)
         {
             var attempts = 0;
+            var exceptions = new List<Exception>();
             Action<Exception> handleFailure = ex =>
             {
                 attempts++;
+
+                if (ex != null) exceptions.Add(ex);
+
                 if (attempts >= allowedRetries)
                 {
-                    throw ex ??
-                        new OpenCircuitException("The operation exhausted all possible retry opportunities while waiting for the circuit breaker to close.");
+                    if (exceptions.Any())
+                        throw new AggregateException("The operation exhausted all possible retry opportunities.", exceptions);
+
+                    throw new OperationFailedException("The operation exhausted all possible retry opportunities.", ex);
                 }
+
                 Thread.Sleep(retryInterval);
             };
             while (attempts < allowedRetries)
---
+++ Tests/RetryTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using ReliabilityPatterns;
+
+namespace Tests
+{
+    [TestFixture]
+    public class RetryTests
+    {
+        [Test]
+        public void ShouldThrowAggregateExceptionWhenMultipleRetriesFailWithExceptions()
+        {
+            var circuitBreaker = new CircuitBreaker(100, TimeSpan.FromSeconds(1));
+            try
+            {
+                circuitBreaker.ExecuteWithRetries(
+                    () => { throw new Exception("foo"); },
+                    5,
+                    TimeSpan.Zero);
+
+                Assert.Fail("No exception was thrown");
+            }
+            catch (AggregateException ex)
+            {
+                Assert.AreEqual(5, ex.InnerExceptions.Count);
+                Assert.IsTrue(ex.InnerExceptions.All(e => e.Message == "foo"));
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<AggregateException>(ex);
+            }
+        }
+    }
+}
