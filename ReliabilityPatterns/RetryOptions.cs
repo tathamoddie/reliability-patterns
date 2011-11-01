3b67231684b83f66f4dc9f806e8c8e6ca91460c4
Tatham Oddie <tatham@oddie.com.au>
2011-11-01 18:39:13 +1100

    Introduced new RetryOptions class.

---
+++ ReliabilityPatterns/RetryOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ReliabilityPatterns
+{
+    public class RetryOptions
+    {
+        public RetryOptions()
+        {
+            AllowedRetries = 12;
+            RetryInterval = TimeSpan.FromSeconds(5);
+        }
+
+        public ushort AllowedRetries { get; set; }
+        public TimeSpan RetryInterval { get; set; }
+    }
+}
