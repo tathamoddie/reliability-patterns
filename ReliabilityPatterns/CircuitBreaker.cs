6a9bac66bb452af14cadc72cf9272769c2073933
James <james.g.brayton@gmail.com>
2016-08-10 07:56:53 -0600

    CircuitBreaker class now inherits from interface for later DI and container use.  Class was also cleaned up to meet coding standards.[C

--- ReliabilityPatterns/CircuitBreaker.cs
+++ ReliabilityPatterns/CircuitBreaker.cs
@@ -1,83 +1,145 @@
-﻿using System;
-using System.Threading;
-using System.Timers;
-using Timer = System.Timers.Timer;
+﻿// -----------------------------------------------------------------------------
+// <summary>
+//     This file contains the CircuitBreaker class.
+// </summary>
+// -----------------------------------------------------------------------------
 
 namespace ReliabilityPatterns
 {
-    public class CircuitBreaker
-    {
-        readonly Timer resetTimer;
-        int failureCount;
-        CircuitBreakerState state;
-        uint threshold;
+    using System;
+    using System.Threading;
+    using System.Timers;
 
+    using Timer = System.Timers.Timer;
+
+    /// <summary>
+    ///     The circuit breaker class.
+    /// </summary>
+    public class CircuitBreaker : ICircuitBreaker
+    {
+        /// <summary>
+        ///     The reset timer.
+        /// </summary>
+        private readonly Timer resetTimer;
+
+        /// <summary>
+        ///     The failure count.
+        /// </summary>
+        private int failureCount;
+
+        /// <summary>
+        ///     The error threshold before the circuit trips.
+        /// </summary>
+        private uint threshold;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="CircuitBreaker"/> class.
+        /// </summary>
         public CircuitBreaker()
             : this(5, TimeSpan.FromSeconds(60))
         {
         }
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="CircuitBreaker"/> class.
+        /// </summary>
+        /// <param name="threshold">
+        ///     The threshold.
+        /// </param>
+        /// <param name="resetTimeout">
+        ///     The reset timeout.
+        /// </param>
         public CircuitBreaker(uint threshold, TimeSpan resetTimeout)
         {
             this.threshold = threshold;
-            failureCount = 0;
-            state = CircuitBreakerState.Closed;
+            this.failureCount = 0;
+            this.State = CircuitBreakerState.Closed;
 
-            resetTimer = new Timer(resetTimeout.TotalMilliseconds);
-            resetTimer.Elapsed += ResetTimerElapsed;
+            this.resetTimer = new Timer(resetTimeout.TotalMilliseconds);
+            this.resetTimer.Elapsed += this.ResetTimerElapsed;
         }
 
+        public event EventHandler StateChanged;
+
+        public event EventHandler ServiceLevelChanged;
+
         /// <summary>
-        /// Number of failures allowed before the circuit trips.
+        ///     Gets or sets the number of failures allowed before the circuit trips.
         /// </summary>
         public uint Threshold
         {
-            get { return threshold; }
+            get
+            {
+                return this.threshold;
+            }
+
             set
             {
                 if (value <= 0)
+                {
                     throw new ArgumentException("Threshold must be greater than zero");
-                
-                threshold = value;
+                }
+
+                this.threshold = value;
             }
         }
 
         /// <summary>
-        /// The time before the circuit attempts to close after being tripped.
+        ///     Gets or sets the time before the circuit attempts to close after being tripped.
         /// </summary>
         public TimeSpan ResetTimeout
         {
-            get { return TimeSpan.FromMilliseconds(resetTimer.Interval); }
-            set { resetTimer.Interval = value.TotalMilliseconds; }
+            get
+            {
+                return TimeSpan.FromMilliseconds(this.resetTimer.Interval);
+            }
+
+            set
+            {
+                this.resetTimer.Interval = value.TotalMilliseconds;
+            }
         }
 
         /// <summary>
-        /// The current service level of the circuit represented as as percentage.
-        /// 0 means it's offline. 100 means everything is ok.
+        ///     Gets the current service level of the circuit represented as as percentage.
+        ///     0 means it's offline. 100 means everything is ok.
         /// </summary>
-        public double ServiceLevel
-        {
-            get { return ((threshold - (double) failureCount)/threshold)*100; }
-        }
+        public double ServiceLevel => ((this.threshold - (double)this.failureCount) / this.threshold) * 100;
 
-        public CircuitBreakerState State
-        {
-            get { return state; }
-        }
+        /// <summary>
+        ///     Gets the current state of the circuit.
+        /// </summary>
+        public CircuitBreakerState State { get; private set; }
 
-        public bool AllowedToAttemptExecute
-        {
-            get { return State == CircuitBreakerState.Closed || State == CircuitBreakerState.HalfOpen; }
-        }
+        /// <summary>
+        ///     Gets a value indicating whether or not the call through the circuit is allowed to
+        ///     attempt to execute.
+        /// </summary>
+        public bool AllowedToAttemptExecute => 
+            this.State == CircuitBreakerState.Closed 
+            || this.State == CircuitBreakerState.HalfOpen;
 
-        public event EventHandler StateChanged;
-        public event EventHandler ServiceLevelChanged;
-
+        /// <summary>
+        ///     Attempts to execute the requested operation through the circuit.
+        /// </summary>
+        /// <typeparam name="TResult">
+        ///     The type of the requested operation.
+        /// </typeparam>
+        /// <param name="operation">
+        ///     The operation to attempt to execute.
+        /// </param>
+        /// <returns>
+        ///     The result of the requested operation.
+        /// </returns>
         public TResult Execute<TResult>(Func<TResult> operation)
         {
-            if (state == CircuitBreakerState.Open)
+            // Check if the circuit is already open.
+            if (this.State == CircuitBreakerState.Open)
+            {
                 throw new OpenCircuitException("Circuit breaker is currently open");
+            }
 
+            // Result to eventually return.
             TResult result;
 
             try
@@ -87,48 +149,54 @@ namespace ReliabilityPatterns
             }
             catch (Exception ex)
             {
-                if (state == CircuitBreakerState.HalfOpen)
+                if (this.State == CircuitBreakerState.HalfOpen)
                 {
                     // Operation failed in a half-open state, so reopen circuit
-                    Trip();
+                    this.Trip();
                 }
-                else if (failureCount < threshold)
+                else if (this.failureCount < this.threshold)
                 {
                     // Operation failed in an open state, so increment failure count and throw exception
-                    Interlocked.Increment(ref failureCount);
+                    Interlocked.Increment(ref this.failureCount);
 
-                    OnServiceLevelChanged(new EventArgs());
+                    this.OnServiceLevelChanged(new EventArgs());
                 }
-                else if (failureCount >= threshold)
+                else if (this.failureCount >= this.threshold)
                 {
                     // Failure count has reached threshold, so trip circuit breaker
-                    Trip();
+                    this.Trip();
                 }
 
                 throw new OperationFailedException("Operation failed", ex);
             }
 
-            if (state == CircuitBreakerState.HalfOpen)
+            if (this.State == CircuitBreakerState.HalfOpen)
             {
                 // If operation succeeded without error and circuit breaker 
                 // is in a half-open state, then reset
-                Reset();
+                this.Reset();
             }
 
-            if (failureCount > 0)
+            if (this.failureCount > 0)
             {
                 // Decrement failure count to improve service level
-                Interlocked.Decrement(ref failureCount);
+                Interlocked.Decrement(ref this.failureCount);
 
-                OnServiceLevelChanged(new EventArgs());
+                this.OnServiceLevelChanged(new EventArgs());
             }
 
             return result;
         }
 
+        /// <summary>
+        ///     Attempts to execute the requested operation through the circuit.
+        /// </summary>
+        /// <param name="operation">
+        ///     The operation to attempt to execute.
+        /// </param>
         public void Execute(Action operation)
         {
-            Execute<object>(() =>
+            this.Execute<object>(() =>
             {
                 operation();
                 return null;
@@ -136,50 +204,63 @@ namespace ReliabilityPatterns
         }
 
         /// <summary>
-        /// Trips the circuit breaker. If the circuit breaker is already open,
-        /// this method has no effect.
+        ///     Trips the circuit breaker. If the circuit breaker is already open,
+        ///     this method has no effect.
         /// </summary>
         public void Trip()
         {
-            if (state == CircuitBreakerState.Open) return;
-            ChangeState(CircuitBreakerState.Open);
-            resetTimer.Start();
+            if (this.State == CircuitBreakerState.Open)
+            {
+                return;
+            }
+
+            this.ChangeState(CircuitBreakerState.Open);
+
+            this.resetTimer.Start();
         }
 
         /// <summary>
-        /// Resets the circuit breaker. If the circuit breaker is already closed,
-        /// this method has no effect.
+        ///     Resets the circuit breaker. If the circuit breaker is already closed,
+        ///     this method has no effect.
         /// </summary>
         public void Reset()
         {
-            if (state == CircuitBreakerState.Closed) return;
-            ChangeState(CircuitBreakerState.Closed);
-            resetTimer.Stop();
+            if (this.State == CircuitBreakerState.Closed)
+            {
+                return;
+            }
+
+            this.ChangeState(CircuitBreakerState.Closed);
+
+            this.resetTimer.Stop();
         }
 
-        void ChangeState(CircuitBreakerState newState)
+        private void ChangeState(CircuitBreakerState newState)
         {
-            state = newState;
-            OnCircuitBreakerStateChanged(new EventArgs());
+            this.State = newState;
+            this.OnCircuitBreakerStateChanged(new EventArgs());
         }
 
-        void ResetTimerElapsed(object sender, ElapsedEventArgs e)
+        private void ResetTimerElapsed(object sender, ElapsedEventArgs e)
         {
-            if (State != CircuitBreakerState.Open) return;
-            ChangeState(CircuitBreakerState.HalfOpen);
-            resetTimer.Stop();
+            if (this.State != CircuitBreakerState.Open)
+            {
+                return;
+            }
+
+            this.ChangeState(CircuitBreakerState.HalfOpen);
+
+            this.resetTimer.Stop();
         }
 
-        void OnCircuitBreakerStateChanged(EventArgs e)
+        private void OnCircuitBreakerStateChanged(EventArgs e)
         {
-            if (StateChanged != null)
-                StateChanged(this, e);
+            this.StateChanged?.Invoke(this, e);
         }
 
-        void OnServiceLevelChanged(EventArgs e)
+        private void OnServiceLevelChanged(EventArgs e)
         {
-            if (ServiceLevelChanged != null)
-                ServiceLevelChanged(this, e);
+            this.ServiceLevelChanged?.Invoke(this, e);
         }
     }
 }
